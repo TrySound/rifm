22a7d937b84cd17be1f3aa8860d681204460a020
Bogdan Chadkin <opensource@trysound.io>
2026-08-12 19:30:35 +0200

    Remove superseded Next.js website

--- website/src/syntax-highlighting.ts
+++
@@ -1,9 +0,0 @@
-import Prism from "prismjs";
-import "prismjs/components/prism-typescript";
-import "prismjs/components/prism-jsx";
-import "prismjs/components/prism-tsx";
-
-export const highlightCode = (code: string, language: "typescript" | "tsx") =>
-  Prism.highlight(code, Prism.languages[language], language);
-
-export const highlightAll = () => Prism.highlightAll();
